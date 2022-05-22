d08d703a4d9e6fe23c36e59330c37dede8477329
clapscofield <clarisse.scofield@gmail.com>
2022-05-22 18:03:15 -0300

    Implementando o Novo Projeto: View para Adicionar Items em uma Lista Existente

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -107,3 +107,31 @@ class NewVisitorTest(LiveServerTestCase):
             self.assertIn('Comprar leite', page_text)
 
             # Satisfeitos, ambos vão dormir
+            
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
