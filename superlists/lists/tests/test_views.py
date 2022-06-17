b41e9845d4de1a69b635779b6df87164e4248ec0
clapscofield <clarisse.scofield@gmail.com>
2022-06-17 15:08:56 -0300

    Validando os erros na view

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-
+from django.utils.html import escape
 from lists.views import home_page
 from lists.models import Item, List
 
@@ -22,6 +22,13 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html') 
 
 class ListViewTest(TestCase):
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 
     def test_uses_list_template(self):
         my_list = List.objects.create()
