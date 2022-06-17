1cbf25c8e585c2987bbc1382a01b2a712ed79000
clapscofield <clarisse.scofield@gmail.com>
2022-06-17 15:47:16 -0300

    Modelo de usuário mínimo

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Application definition
 
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lists'
+    'lists',
+    'accounts'
 ]
 
 MIDDLEWARE = [
